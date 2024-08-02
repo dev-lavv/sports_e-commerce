8cbebf91d96ab8ec891cf25c1989fc624a914e04
Anku5x <ankushnagdev@outlook.com>
2024-08-03 02:45:58 +0530

    v0.0 WSOE Fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home.jsx"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
 function App() {
   return (
     <>
-    <Home />
+    <Home/>
+    
     </>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,17 +2,14 @@ import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { useState, useEffect } from "react";
-
 const Home = () => {
-  const [firstImgState, setFirstImg] = useState(imgOne);
-  const [firstHeading, setFirstHeading] = useState("Bats");
   const imgOne =
-    "https://dkpcricketonline.com/cdn/shop/files/DKP_Range_Sqaure_1_3024x.jpg?v=1706124419";
-  const imgTwo =
-    "https://dkpcricketonline.com/cdn/shop/files/thumbnail_IMG_0046_1296x.jpg?v=1694374322";
-  const imgThree =
-    "https://www.gray-nicolls.co.uk/cdn/shop/files/GettyImages-1964812307.jpg?v=1720794694&width=1800";
-
+  "https://dkpcricketonline.com/cdn/shop/files/DKP_Range_Sqaure_1_3024x.jpg?v=1706124419";
+const imgTwo =
+  "https://dkpcricketonline.com/cdn/shop/files/thumbnail_IMG_0046_1296x.jpg?v=1694374322";
+const imgThree =
+  "https://www.gray-nicolls.co.uk/cdn/shop/files/GettyImages-1964812307.jpg?v=1720794694&width=1800";
+  const [firstImgState, setFirstImg] = useState(imgOne);
   const handleFirstLeft = () => {
     if (firstImgState === imgThree) {
       setFirstImg(imgTwo);
@@ -35,21 +32,19 @@ const Home = () => {
     let timer1 = setTimeout(() => {
       if (firstImgState === imgOne) {
         setFirstImg(imgTwo);
-        setFirstHeading("Balls");
       } else if (firstImgState === imgTwo) {
         setFirstImg(imgThree);
-        setFirstHeading("Helmets");
       } else if (firstImgState === imgThree) {
         setFirstImg(imgOne);
-        setFirstHeading("Bats");
       }
     }, 3000);
     return () => {
       clearTimeout(timer1);
     };
   }, [firstImgState]);
-  return (
-    <>
+
+    return (
+      <>
       <div className="fixed w-full lg:h-[50px] h-[45px] flex justify-center border-b-2 border-custom-yellow bg-white ">
         <Header home="border-b-2 border-custom-yellow" />
       </div>
@@ -71,7 +66,7 @@ const Home = () => {
               left: `${"30%"}`,
             }}
           >
-            Cricket {firstHeading}
+            Cricket Bats
           </h1>
         </a>
         <div className="absolute top-1/2">
@@ -95,7 +90,7 @@ const Home = () => {
 
       <Footer />
     </>
-  );
-};
-
-export default Home;
+    );
+  };
+  
+  export default Home;
