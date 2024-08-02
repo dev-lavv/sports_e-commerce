a9c02f0471673ba0ee9d993bffe8e546bf79fe94
Anku5x <ankushnagdev@outlook.com>
2024-08-03 02:47:46 +0530

    v0.0 WSOD Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import Home from "./pages/Home.jsx"
-import Header from "./components/Header.jsx"
-import Footer from "./components/Footer.jsx"
 function App() {
   return (
     <>
