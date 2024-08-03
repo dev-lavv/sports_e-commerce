d9e367434d0932cc94bb8395e6af2e369b7a3340
Anku5x <ankushnagdev@outlook.com>
2024-08-03 13:02:01 +0530

    v0.0

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,39 +3,53 @@ import Footer from "../components/Footer.jsx";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { useState, useEffect } from "react";
 const Home = () => {
+  //variables
   const imgOne =
-  "https://dkpcricketonline.com/cdn/shop/files/DKP_Range_Sqaure_1_3024x.jpg?v=1706124419";
-const imgTwo =
-  "https://dkpcricketonline.com/cdn/shop/files/thumbnail_IMG_0046_1296x.jpg?v=1694374322";
-const imgThree =
-  "https://www.gray-nicolls.co.uk/cdn/shop/files/GettyImages-1964812307.jpg?v=1720794694&width=1800";
+    "https://dkpcricketonline.com/cdn/shop/files/DKP_Range_Sqaure_1_3024x.jpg?v=1706124419";
+  const imgTwo =
+    "https://dkpcricketonline.com/cdn/shop/files/thumbnail_IMG_0046_1296x.jpg?v=1694374322";
+  const imgThree =
+    "https://www.gray-nicolls.co.uk/cdn/shop/files/GettyImages-1964812307.jpg?v=1720794694&width=1800";
+  //states/props
   const [firstImgState, setFirstImg] = useState(imgOne);
+  const [h1TextState, setH1] = useState("balls");
+  //functions
   const handleFirstLeft = () => {
     if (firstImgState === imgThree) {
       setFirstImg(imgTwo);
+      setH1("bats");
     } else if (firstImgState === imgTwo) {
       setFirstImg(imgOne);
+      setH1("balls");
     } else {
       setFirstImg(imgThree);
+      setH1("helmets");
     }
   };
   const handleFirstRight = () => {
     if (firstImgState === imgOne) {
       setFirstImg(imgTwo);
+      setH1("bats");
     } else if (firstImgState === imgTwo) {
       setFirstImg(imgThree);
+      setH1("helmets");
     } else {
       setFirstImg(imgOne);
+      setH1("balls");
     }
   };
+  //hooks
   useEffect(() => {
     let timer1 = setTimeout(() => {
       if (firstImgState === imgOne) {
         setFirstImg(imgTwo);
+        setH1("bats");
       } else if (firstImgState === imgTwo) {
         setFirstImg(imgThree);
+        setH1("helmets");
       } else if (firstImgState === imgThree) {
         setFirstImg(imgOne);
+        setH1("balls");
       }
     }, 3000);
     return () => {
@@ -43,8 +57,8 @@ const imgThree =
     };
   }, [firstImgState]);
 
-    return (
-      <>
+  return (
+    <>
       <div className="fixed w-full lg:h-[50px] h-[45px] flex justify-center border-b-2 border-custom-yellow bg-white ">
         <Header home="border-b-2 border-custom-yellow" />
       </div>
@@ -66,7 +80,7 @@ const imgThree =
               left: `${"30%"}`,
             }}
           >
-            Cricket Bats
+            Cricket {h1TextState}
           </h1>
         </a>
         <div className="absolute top-1/2">
@@ -87,10 +101,9 @@ const imgThree =
         </div>
       </div>
       <div className="w-full h-screen"></div>
-
       <Footer />
     </>
-    );
-  };
-  
-  export default Home;
+  );
+};
+
+export default Home;
