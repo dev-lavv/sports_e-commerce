d2b9ddbb766d51c9be9f6e1e20b494d89dba1fc7
Anku5x <ankushnagdev@outlook.com>
2024-08-16 00:57:00 +0530

    v0.0
    Installed react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <Home/>
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
